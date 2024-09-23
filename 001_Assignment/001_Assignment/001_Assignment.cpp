05221ad34b4a196dbcaa2c7ae937743b42412d99
JeonHyeJu <xcpc7@naver.com>
2024-09-23 14:28:33 +0900

    add assignment 01

---
+++ 001_Assignment/001_Assignment/001_Assignment.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int StringCount(const char* const _Str)
+{
+    if (!_Str) return 0;
+
+    int Count = 0;
+    while (_Str[Count])
+    {
+        if (_Str[Count] == '\0') {
+            break;
+        }
+
+        Count++;
+    }
+
+    return Count;
+}
+
+int main()
+{
+    // [B][B][B][B][0]
+
+    const char* temp = "BBBB";
+    int Count = strlen(temp);
+
+    std::cout << "original: " << Count << std::endl;
+
+    Count = StringCount(temp);
+    std::cout << "new: " << Count << std::endl;
+}
