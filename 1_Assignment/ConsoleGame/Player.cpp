e99118a5a811ccea3c32f320a6cf68e808d02eab
JeonHyeJu <xcpc7@naver.com>
2024-10-11 16:04:16 +0900

    JHJ: 241011 moving

---
+++ 1_Assignment/ConsoleGame/Player.cpp
@@ -0,0 +1,94 @@
+#include "Player.h"
+#include <conio.h>
+#include "Enums.h"
+
+void Player::BeginPlay()
+{
+	PlayerImage.Create({1, 1}, '@');
+}
+
+void Player::Tick()
+{
+	Move();
+}
+
+void Player::SetBackScreenSize(const FSize& size)
+{
+	mMaxBackScreenSize = size;
+}
+
+void Player::Move()
+{
+	int Value = _kbhit();
+	Enums::GAMEDIR Dir = Enums::GAMEDIR::NONE;
+
+	if (Value != 0)
+	{
+		int Select = _getch();
+
+		switch (Select)
+		{
+		case 'A':
+		case 'a':
+			Dir = Enums::GAMEDIR::LEFT;
+			break;
+		case 'D':
+		case 'd':
+			Dir = Enums::GAMEDIR::RIGHT;
+			break;
+		case 'W':
+		case 'w':
+			Dir = Enums::GAMEDIR::UP;
+			break;
+		case 'S':
+		case 's':
+			Dir = Enums::GAMEDIR::DOWN;
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	FIntPoint futurePos = Pos;
+
+	switch (Dir)
+	{
+	case Enums::GAMEDIR::LEFT:
+		futurePos += FIntPoint::LEFT;
+		break;
+	case Enums::GAMEDIR::RIGHT:
+		futurePos += FIntPoint::RIGHT;
+		break;
+	case Enums::GAMEDIR::UP:
+		futurePos += FIntPoint::UP;
+		break;
+	case Enums::GAMEDIR::DOWN:
+		futurePos += FIntPoint::DOWN;
+		break;
+	default:
+		break;
+	}
+
+	if (0 > futurePos.X || futurePos.X >= mMaxBackScreenSize.w)
+	{
+		return;
+	}
+	if (0 > futurePos.Y || futurePos.Y >= mMaxBackScreenSize.h)
+	{
+		return;
+	}
+
+	Pos = futurePos;
+}
+
+void Player::Render(ConsoleImage* _BackBuffer)
+{
+	// delete _BackBuffer;
+	_BackBuffer->Copy(Pos, PlayerImage);
+}
+
+void Player::SetActorLocation(FIntPoint _Pos)
+{
+	Pos = _Pos;
+}
